b9ec087ac65960a86624b865e61442b97aefe22c
Tekuri Sai Akhil <st5050@nyu.edu>
2024-06-17 21:20:26 +0530

    updated request body with document urls

--- app/features/dynamo/core.py
+++ app/features/dynamo/core.py
@@ -1,14 +1,18 @@
 from features.dynamo.tools import summarize_transcript, summarize_pdf, generate_flashcards
 from services.logger import setup_logger
 from api.error_utilities import VideoTranscriptError
+from services.tool_registry import ToolFile
 
 logger = setup_logger(__name__)
 
-def executor(youtube_url: str, pdf_file_url:str, verbose=False):
+def executor(youtube_url: str, document_urls: list[str], verbose=False):
     if len(youtube_url)!=0:
         summary = summarize_transcript(youtube_url, verbose=verbose)
-    elif len(pdf_file_url)!=0:
-        summary = summarize_pdf(pdf_file_url, verbose=verbose)
+    elif len(document_urls)!=0:
+        summary = ""
+        for url in document_urls:
+            if url.split(".")[-1]=="pdf":
+                summary = summary +"\n" + summarize_pdf(url, verbose=verbose)
     else:
         summary = ""
     
