f99c28cb8b8a317e9bb4998114ebdfcf74cfa35d
Tekuri Sai Akhil <st5050@nyu.edu>
2024-07-23 12:13:09 +0530

    worksheet generator done

--- app/features/worksheet_generator/core.py
+++ app/features/worksheet_generator/core.py
@@ -1,8 +1,15 @@
 from app.services.logger import setup_logger
 from app.services.tool_registry import WorksheetQuestionModel
+from app.features.worksheet_generator.tools import get_course_type, generate_worksheet
+from app.features.worksheet_generator.document_loaders import get_docs
 logger = setup_logger(__name__)
 
 def executor(topic: str, grade_level: str, worksheet_list: list[WorksheetQuestionModel], file_type: str, file_url: str, verbose=True):
    
 
-    return "Worksheet generator" 
+    course_type = get_course_type(topic)
+    docs = get_docs(file_url, file_type)
+    worksheet = generate_worksheet(course_type, grade_level, worksheet_list, docs)
+
+    return worksheet
+    
